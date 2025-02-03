ece44f6b5325c947a03f928b386d08e18d106f59
Mamura <marcio.mota@gmail.com>
2025-02-03 00:16:57 -0300

    adicionado imagens ASCII para ganhador e perdedor

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ O jogo consiste em o programa escolher um número secreto e os jogadores tentare
 ## ⚙️ Funcionalidades do Sistema:
 - O Jogo pedindo para o primeiro jogador escolher um número entre 0 e 100.
 - Cada jogador deve digitar um numero inteiro tentando acertar o número secreto.
-- Caso o jogador erre a tentativa o probrama deve diminuir o range de escolhas para os valores 0 ou 100 e o valor da tentativa do ultimo jogador.
+- Caso o jogador erre a tentativa o programa deve diminuir o range de escolhas para os valores 0 ou 100 e o valor da tentativa do ultimo jogador.
 - As rodadas deverão continuar até um jogador acertar o número secreto.
 
 ### Acadêmico
--- zero-cem.c
+++ zero-cem.c
@@ -14,20 +14,95 @@ void clear()
 void drawHeader()
 {
     printf("\n\n");
-    printf("*********************************\n");
-    printf("*              ___              *\n");
-    printf("*    JOGO     |[_]|     JOGO    *\n");
-    printf("*   0 A 100   |+ ;|    0 A 100  *\n");
-    printf("*             '---'             *\n");
-    printf("*********************************\n");
+    printf("*******************************************\n");
+    printf("*                   ___                   *\n");
+    printf("*         JOGO     |[_]|     JOGO         *\n");
+    printf("*        0 A 100   |+ ;|    0 A 100       *\n");
+    printf("*                  '---'                  *\n");
+    printf("*******************************************\n");
+}
+
+void drawWinner(int player)
+{
+    printf("*******************************************\n");
+    printf("*            PARABENS JOGADOR %d           *\n", player);
+    printf("*              VOCÊ VENCEU                *\n");
+    printf("*            |@@@@|     |####|            *\n");
+    printf("*            |@@@@|     |####|            *\n");
+    printf("*            |@@@@|     |####|            *\n");
+    printf("*            \\@@@@|     |####/            *\n");
+    printf("*             \\@@@|     |###/             *\n");
+    printf("*              `@@|_____|##'              *\n");
+    printf("*                   (O)                   *\n");
+    printf("*                .-'''''-.                *\n");
+    printf("*              .'  * * *  `.              *\n");
+    printf("*             :  *       *  :             *\n");
+    printf("*            : ~  CAMPEÃO  ~ :            *\n");
+    printf("*            : ~  0 A 100  ~ :            *\n");
+    printf("*             :  *       *  :             *\n");
+    printf("*              `.  * * *  .'              *\n");
+    printf("*                `-.....-'                *\n");
+    printf("*******************************************\n");
+}
+
+void drawLooser(int player)
+{
+    printf("**********************************************\n");
+    printf("*           ／＞     フ                      *\n");
+    printf("*　　　　　 | 　_　 _|                       *\n");
+    printf("*　 　　　／` ミ＿xノ                        *\n");
+    printf("*　　 　 /　　　 　 | VOCÊ PERDEU JOGADOR %d  *\n", player);
+    printf("*　　　 /　 ヽ　　 ﾉ                         *\n");
+    printf("*　 　 │　　|　|　|                          *\n");
+    printf("*　／￣|　　 |　|　|                         *\n");
+    printf("*　| (￣ヽ＿ヽ)__) __)                       *\n");
+    printf("*　＼二つ                                    *\n");
+    printf("**********************************************\n");
 }
 
 void main() {
     int players;
+    int initRange = 0, endRange = 100;
+    int kick = 0;
 
     srand(time(0));
     int secretNumber = rand() % 100;
 
     clear();
     drawHeader();
+printf("%d", secretNumber);
+    printf("# Adicione a quantidade de jogadores: ");
+    scanf("%d", &players);
+    printf("\n\n");
+
+    while (secretNumber != kick)
+    {
+        for(int i = 1; i <= players; i++) {
+            printf("## JOGADOR %d ##\n", i);
+            printf("# Digite um numero de %d a %d: ", initRange, endRange);
+            scanf("%d", &kick);
+            printf("\n\n");
+    
+            if (kick == secretNumber) {
+                drawWinner(i);
+
+                continue;
+            }
+
+            // Chute Fora do Range
+            if (kick < initRange || kick > endRange) {
+                drawLooser(i);
+                kick = secretNumber;
+
+                continue;
+            }
+
+            if (kick < secretNumber) {
+                initRange = kick;
+            } else {
+                endRange = kick;
+            }
+        }
+    }
+    
 }
